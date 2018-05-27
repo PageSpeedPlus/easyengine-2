9d14a3ce13fd75008125527cf0bf1c4ae500d158
Daniel Bieli <22905324+danielbieli@users.noreply.github.com>
2018-05-27 14:09:10 +0200

    Update nginx-geoip-update.sh

--- home/tools/nginx-geoip-update.sh
+++ home/tools/nginx-geoip-update.sh
@@ -7,12 +7,13 @@
 # Date: 2018-05-27
 # Author: Daniel Bieli <danibieli.1185@gmail.com>
 # Description: NGiNX - Geo IP wöchentlich updaten. https://www.maxmind.com/de/geoip2-services-and-databases
-#
+
 # Download Country IP database
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 gunzip GeoIP.dat.gz
 rm -f /usr/share/GeoIP/GeoIP.dat
 mv GeoIP.dat /usr/share/GeoIP/GeoIP.dat
+
 # Download City IP database
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 gunzip GeoLiteCity.dat.gz
