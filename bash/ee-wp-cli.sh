45d114d0df711af8d2285de5f5831656d74f7159
Daniel Bieli <22905324+danielbieli@users.noreply.github.com>
2018-06-10 13:22:12 +0200

    Update ee-wp-cli.sh

--- bash/ee-wp-cli.sh
+++ bash/ee-wp-cli.sh
@@ -3,6 +3,11 @@
 # | WordPress Auto Konfiguration
 # ===================================================================================
 
+echo ""
+echo "Domain eingeben!"
+echo ""
+read -r domain
+
 # Ins WordPress Verzeichnis wechseln
 cd /var/www/$domain
 cd htdocs
@@ -42,10 +47,6 @@ wp option update blogdescription "Professional Performance"
 # wp option update admin_email someone@example.com
 # wp option update default_role author
 
-# PHP Cron Job durch Linux Systen Cron ersetzen
-crontab -e
-*/15 * * * * curl https://wpnginx.tk/wp-cron.php?doing_wp_cron > /dev/null 2>&1
-
 # Kommentare per Standart deaktiviert
 wp option set default_comment_status closed;
 
@@ -63,10 +64,9 @@ wp widget delete $(wp widget list sidebar-1 --format=ids);
 wp plugin install --activate code-snippets worker optimus query-monitor search-by-algolia-instant-relevant-results responsify-wp snitch autodescription updraftplus vevida-optimizer ninjafirewall
 # wp plugin install disqus-conditional-load elasticpress favicon-by-realfavicongenerator lazy-load-for-comments p3-profiler pods searchwp-api secsign table-of-contents-plus tablepress the-events-calendar wp-ultimate-csv-importer wp-external-links wp-sweep
 
-
 # Kommentare und Pingbacks deaktivieren
-wp post list --format=ids | xargs wp post update --comment_status=closed
-wp post list --format=ids | xargs wp post update --ping_status=closed
+# wp post list --format=ids | xargs wp post update --comment_status=closed
+# wp post list --format=ids | xargs wp post update --ping_status=closed
 
 # Dateiberechtigungen setzten
 chown -R www-data:www-data /var/www
@@ -75,38 +75,40 @@ find /var/www/ -type f -exec chmod 644 {} \;
 cd /var/www/$domain
 chmod 400 wp-config.php
 
+# PHP Cron Job durch Linux Systen Cron ersetzen
+echo "*/15 * * * * curl https://wpnginx.tk/wp-cron.php?doing_wp_cron > /dev/null 2>&1"
+crontab -e
 
 
 
 
+#wp core config --extra-php <<PHP define( 'WP_POST_REVISIONS', false ); PHP
+#wp core config --extra-php <<PHP define( 'EMPTY_TRASH_DAYS', 2 ); PHP
+#wp core config --extra-php <<PHP define( 'AUTOSAVE_INTERVAL', 90 ); PHP
+#wp core config --extra-php <<PHP define( 'DISABLE_WP_CRON', true ); PHP
+#wp core config --extra-php <<PHP define( 'WPLANG', 'de_DE' ); PHP
 
-wp core config --extra-php <<PHP define( 'WP_POST_REVISIONS', false ); PHP
-wp core config --extra-php <<PHP define( 'EMPTY_TRASH_DAYS', 2 ); PHP
-wp core config --extra-php <<PHP define( 'AUTOSAVE_INTERVAL', 90 ); PHP
-wp core config --extra-php <<PHP define( 'DISABLE_WP_CRON', true ); PHP
-wp core config --extra-php <<PHP define( 'WPLANG', 'de_DE' ); PHP
+#wp core config --extra-php <<PHP define( 'UPLOADS', 'https://$domain/assets/img' ); PHP
 
-wp core config --extra-php <<PHP define( 'UPLOADS', 'https://$domain/assets/img' ); PHP
+#wp core config --extra-php <<PHP define( 'FS_CHMOD_DIR', ( 0755 & ~ umask() ) ); PHP
+#wp core config --extra-php <<PHP define( 'FS_CHMOD_FILE', ( 0644 & ~ umask() ) ); PHP
 
-wp core config --extra-php <<PHP define( 'FS_CHMOD_DIR', ( 0755 & ~ umask() ) ); PHP
-wp core config --extra-php <<PHP define( 'FS_CHMOD_FILE', ( 0644 & ~ umask() ) ); PHP
+#wp core config --extra-php <<PHP define( 'WP_MEMORY_LIMIT', '256M' ); PHP
+#wp core config --extra-php <<PHP define( 'WP_MAX_MEMORY_LIMIT', '512M' ); PHP
 
-wp core config --extra-php <<PHP define( 'WP_MEMORY_LIMIT', '256M' ); PHP
-wp core config --extra-php <<PHP define( 'WP_MAX_MEMORY_LIMIT', '512M' ); PHP
+#wp core config --extra-php <<PHP $table_prefix = 'wp1_'; PHP
 
-wp core config --extra-php <<PHP $table_prefix = 'wp1_'; PHP
+#wp core config --extra-php <<PHP define( 'IMAGE_EDIT_OVERWRITE', true ); PHP
 
-wp core config --extra-php <<PHP define( 'IMAGE_EDIT_OVERWRITE', true ); PHP
+#wp core config --extra-php <<PHP define( 'WP_CACHE', true ); PHP
 
-wp core config --extra-php <<PHP define( 'WP_CACHE', true ); PHP
+#wp core config --extra-php <<PHP define( 'WP_HTTP_BLOCK_EXTERNAL', true ); PHP
+#wp core config --extra-php <<PHP define( 'WP_ACCESSIBLE_HOSTS', 'api.wordpress.org,*.github.com' ); PHP
 
-wp core config --extra-php <<PHP define( 'WP_HTTP_BLOCK_EXTERNAL', true ); PHP
-wp core config --extra-php <<PHP define( 'WP_ACCESSIBLE_HOSTS', 'api.wordpress.org,*.github.com' ); PHP
-
-wp core config --extra-php <<PHP define( 'WP_DEBUG', true ); PHP
-wp core config --extra-php <<PHP define( 'SCRIPT_DEBUG', true ); PHP
-wp core config --extra-php <<PHP define( 'WP_DEBUG_LOG', false ); PHP
-wp core config --extra-php <<PHP define( 'WP_DEBUG_DISPLAY', true ); PHP
+#wp core config --extra-php <<PHP define( 'WP_DEBUG', true ); PHP
+#wp core config --extra-php <<PHP define( 'SCRIPT_DEBUG', true ); PHP
+#wp core config --extra-php <<PHP define( 'WP_DEBUG_LOG', false ); PHP
+#wp core config --extra-php <<PHP define( 'WP_DEBUG_DISPLAY', true ); PHP
 
 
 
