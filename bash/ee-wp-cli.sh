9f920406a4a1813f1757020252e34d35730c211c
Daniel Bieli <22905324+danielbieli@users.noreply.github.com>
2018-06-10 11:28:36 +0200

    Update ee-wp-cli.sh

--- bash/ee-wp-cli.sh
+++ bash/ee-wp-cli.sh
@@ -13,9 +13,14 @@ wp core update
 wp plugin update --all
 wp theme update --all
 
-# Standart Müll entfernen
-wp plugin uninstall akismet hello-dolly
-wp theme uninstall twentyfifteen twentysixteen
+# Sprache ändern
+wp language core install --activate de_DE
+
+# Datum und Zeit anpassen
+wp option update date_format 'Y-m-d'
+wp option update time_format 'H:i'
+wp option update start_of_week 1
+wp option update timezone_string 'Europe/Zurich'
 
 # HTTPS für alle Links
 wp option update home 'https://$domain'
@@ -28,26 +33,17 @@ wp rewrite flush
 # Jahr / Monat Ordner Struktur deaktivieren
 wp option update uploads_use_yearmonth_folders 0
 
-wp option update date_format 'Y-m-d'
-wp option update time_format 'H:i'
-wp option update start_of_week 0
-
-wp option update timezone_string 'Germany/Berlin'
-
-# Sprache ändern
-wp language core install --activate de_DE
-
-# Zeitzone anpassen
-wp option update timezone_string "Germany/Berlin"
-
 # Blog Name und Beschreibung mit vorübergehendem Inhalt füllen
 wp option update blogname "PageSpeed+"
 wp option update blogdescription "Professional Performance"
-
-
-
 # wp option update admin_email someone@example.com
 # wp option update default_role author
+
+
+# Standart Müll entfernen
+wp plugin uninstall akismet hello-dolly
+wp theme uninstall twentyfifteen twentysixteen
+
 
 wp core config --extra-php <<PHP define( 'WP_POST_REVISIONS', false ); PHP
 wp core config --extra-php <<PHP define( 'EMPTY_TRASH_DAYS', 2 ); PHP
